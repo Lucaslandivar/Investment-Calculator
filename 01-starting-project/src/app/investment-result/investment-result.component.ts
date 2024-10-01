354657104e6631d4f844f85ed6e9c3876445de3e
Lucas Landivar <landivarlucas67@gmail.com>
2024-10-01 19:10:12 -0300

    Passing Data from Parent to Child with Inputs

---
+++ 01-starting-project/src/app/investment-result/investment-result.component.ts
@@ -0,0 +1,27 @@
+import { Component, Input, input } from '@angular/core';
+
+@Component({
+  selector: 'app-investment-result',
+  standalone: true,
+  imports: [],
+  templateUrl: './investment-result.component.html',
+  styleUrl: './investment-result.component.css',
+})
+export class InvestmentResultComponent {
+  // results = input<{
+  //   year: number;
+  //   interest: number;
+  //   valueEndOfYear: number;
+  //   annualInvestment: number;
+  //   totalInterest: number;
+  //   totalAmountInvested: number;
+  // }[]>();
+  @Input() results?: {
+    year: number;
+    interest: number;
+    valueEndOfYear: number;
+    annualInvestment: number;
+    totalInterest: number;
+    totalAmountInvested: number;
+  }[];
+}
