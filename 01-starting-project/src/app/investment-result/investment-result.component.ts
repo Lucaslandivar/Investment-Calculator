5a03c0fb97a2dd05002cdf11b46e3f337eb07308
Lucas Landivar <landivarlucas67@gmail.com>
2024-10-02 19:34:45 -0300

    Migrating to Angular Modules

--- 01-starting-project/src/app/investment-result/investment-result.component.ts
+++ 01-starting-project/src/app/investment-result/investment-result.component.ts
@@ -1,11 +1,8 @@
-import { CurrencyPipe } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { InvestmentService } from '../investment.service';
 
 @Component({
   selector: 'app-investment-result',
-  standalone: true,
-  imports: [CurrencyPipe],
   templateUrl: './investment-result.component.html',
   styleUrl: './investment-result.component.css',
 })
